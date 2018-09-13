1da1ad442e7adf4dec121f5f3fd54fd830246cdd
Robert Huff <robertjhuff@gmail.com>
2018-09-13 14:57:35 -0600

    baseline

--- CHAT_BOX.py
+++ CHAT_BOX.py
@@ -7,10 +7,6 @@ sql_transaction = []
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
-d = connection.cursor()
-c = connection.cursor()
-d = connection.cursor()
-
 
 def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
